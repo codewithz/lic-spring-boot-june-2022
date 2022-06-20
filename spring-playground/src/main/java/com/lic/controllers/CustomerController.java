5ac738f95d9396ab1e29f2382ec982049c287017
codewithz <zartab@codewithz.com>
2022-06-20 16:11:55 +0530

    Path Variable

--- spring-playground/src/main/java/com/lic/controllers/CustomerController.java
+++ spring-playground/src/main/java/com/lic/controllers/CustomerController.java
@@ -1,7 +1,11 @@
 package com.lic.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lic.model.Customer;
@@ -10,7 +14,9 @@ import com.lic.services.CustomerService;
 
 
 @RestController
+@RequestMapping("/customers")
 public class CustomerController {
+	
 	@Autowired
 	CustomerService customerService;
 	
@@ -19,10 +25,17 @@ public class CustomerController {
 		// TODO Auto-generated constructor stub
 	}
 
-	@GetMapping
-	public Customer getCustomer() {
-		Customer c=customerService.getCustomer();
+	@GetMapping("/{customerId}")
+	public Customer getCustomer(@PathVariable Long customerId) {
+		Customer c=customerService.getCustomerById(customerId);
 		return c;
 	}
+	
+	@GetMapping
+	public List<Customer> getCustomers(){
+		List<Customer> list=customerService.getCustomers();
+		return list;
+	}
+	
 	
 }
--- spring-playground/src/main/java/com/lic/services/CustomerService.java
+++ spring-playground/src/main/java/com/lic/services/CustomerService.java
@@ -1,5 +1,8 @@
 package com.lic.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.lic.model.Customer;
@@ -7,9 +10,21 @@ import com.lic.model.Customer;
 @Service
 public class CustomerService {
 
-	public Customer getCustomer() {
-		Customer c=new Customer(1L,"James Service Bond");
+	public Customer getCustomerById(Long id) {
+		Customer c=new Customer(id,"James Service Bond");
 		return c;
+	}
+	
+	public List<Customer> getCustomers(){
+		List<Customer> list=new ArrayList<Customer>();
+		
+				list.add(new Customer(1L,"James Bond"));
+				list.add(new Customer(2L,"Sheldon Cooper"));
+				list.add(new Customer(3L,"Elon Musk"));
+				
+		
+		return list;
+	
 	}
 	
 }

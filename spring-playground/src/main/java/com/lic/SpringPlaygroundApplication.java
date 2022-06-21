1d1e42c7af14723b58cc739991f2ef3477e8f775
codewithz <zartab@codewithz.com>
2022-06-21 17:12:46 +0530

    Sorting

--- spring-playground/src/main/java/com/lic/SpringPlaygroundApplication.java
+++ spring-playground/src/main/java/com/lic/SpringPlaygroundApplication.java
@@ -1,11 +1,13 @@
 package com.lic;
 
+import com.github.javafaker.Faker;
 import com.lic.model.Student;
 import com.lic.repository.StudentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
@@ -24,93 +26,120 @@ public class SpringPlaygroundApplication {
 	@Bean
 	CommandLineRunner commandLineRunner(StudentRepository repository){
 		return args -> {
-
-			Student tom = new Student(
-					"Tom", "Smith",
-					"tom.smith@gmail.com", 23);
-			System.out.println("Saving Tom");
-			repository.save(tom);
-
-			Student alex = new Student("Alex", "Holmes",
-					"alex.holmes@gmail.com", 21);
-
-			Student mike = new Student("Mike", "Porter",
-					"mike.porter@gmail.com", 25);
-
-			System.out.println("Saving Mike and Alex");
-
-			repository.saveAll(List.of(mike, alex));
-			System.out.println("-------------------------------------------------");
-			System.out.println("Number of students: " + repository.count());
-			System.out.println("-------------------------------------------------");
-			Optional<Student> optional = repository.findById(2L);
-
-			if (optional.isPresent()) {
-				System.out.println(optional.get());
-			}
-			System.out.println("-------------------------------------------------");
-			repository.findById(1L)
-					.ifPresentOrElse(
-							System.out::println,
-							() -> System.out.println("Student with id 1 is not found")
-					);
-			System.out.println("-------------------------------------------------");
-			repository.findById(5L)
-					.ifPresentOrElse(
-							System.out::println,
-							() -> System.out.println("Student with id 5 is not found")
-					);
-
-			System.out.println("-------------------------------------------------");
-
-			List<Student> students = repository.findAll();
-			students.forEach(System.out::println);
-
-			System.out.println("-------------------------------------------------");
-
-			System.out.println("--- Updating Tom-----");
-
-			Optional<Student> optional1 = repository.findById(1L);
-			if (optional1.isPresent()) {
-				Student tomCopy = optional1.get();
-				System.out.println(tomCopy);
-
-				tomCopy.setFirstName("Thomas");
-				tomCopy.setEmail("thomas.smith@gmail.com");
-
-				repository.save(tomCopy);
-			}
-
-			System.out.println("------------------------------Custom Queries-------------------------");
-
-			repository.findStudentByEmail("thomas.smith@gmail.com")
-					.ifPresentOrElse(
-							System.out::println,
-							()-> System.out.println("Not Found")
-					);
-
-			System.out.println("-----------------------------------------");
-
-			List<Student> list=repository.
-					findStudentsByFirstNameAndAgeGreaterThanEqual("Alex",21);
-
-			list.forEach(System.out::println);
-
-			System.out.println("-----------------------------------------");
-
-			List<Student> list1=repository.
-					findStudentsByFirstNameOrAgeGreaterThanEqual("Alex",21);
-
-			list1.forEach(System.out::println);
-
-			System.out.println("----------- QUERIES----------------------------");
-			repository.selectStudentByEmail("alex.holmes@gmail.com")
-					.ifPresentOrElse(
-							System.out::println,
-							()-> System.out.println("Not Found")
-					);
+		generateRandomStudents(repository);
+			System.out.println("-------------------------------");
+			sorting(repository);
 
 		};
+	}
+
+	private void generateRandomStudents(StudentRepository repository){
+		Faker faker=new Faker();
+
+		for (int i=1;i<=30;i++){
+			String firstName=faker.name().firstName();
+			String lastName=faker.name().lastName();
+			String email=String.format("%s.%s@gmail.com",firstName,lastName);
+			int age=faker.number().numberBetween(18,60);
+			Student s=new Student(firstName,lastName,email,age);
+			repository.save(s);
+		}
+	}
+
+	private void sorting(StudentRepository repository){
+		Sort sort=Sort.by("firstName").ascending()
+						.and(Sort.by("age").descending());
+
+		repository.findAll(sort)
+				.forEach(student -> System.out.println(student.getFirstName()+"--"+student.getAge()));
+	}
+
+	private void jpaBasicQueries(StudentRepository repository){
+		Student tom = new Student(
+				"Tom", "Smith",
+				"tom.smith@gmail.com", 23);
+		System.out.println("Saving Tom");
+		repository.save(tom);
+
+		Student alex = new Student("Alex", "Holmes",
+				"alex.holmes@gmail.com", 21);
+
+		Student mike = new Student("Mike", "Porter",
+				"mike.porter@gmail.com", 25);
+
+		System.out.println("Saving Mike and Alex");
+
+		repository.saveAll(List.of(mike, alex));
+		System.out.println("-------------------------------------------------");
+		System.out.println("Number of students: " + repository.count());
+		System.out.println("-------------------------------------------------");
+		Optional<Student> optional = repository.findById(2L);
+
+		if (optional.isPresent()) {
+			System.out.println(optional.get());
+		}
+		System.out.println("-------------------------------------------------");
+		repository.findById(1L)
+				.ifPresentOrElse(
+						System.out::println,
+						() -> System.out.println("Student with id 1 is not found")
+				);
+		System.out.println("-------------------------------------------------");
+		repository.findById(5L)
+				.ifPresentOrElse(
+						System.out::println,
+						() -> System.out.println("Student with id 5 is not found")
+				);
+
+		System.out.println("-------------------------------------------------");
+
+		List<Student> students = repository.findAll();
+		students.forEach(System.out::println);
+
+		System.out.println("-------------------------------------------------");
+
+		System.out.println("--- Updating Tom-----");
+
+		Optional<Student> optional1 = repository.findById(1L);
+		if (optional1.isPresent()) {
+			Student tomCopy = optional1.get();
+			System.out.println(tomCopy);
+
+			tomCopy.setFirstName("Thomas");
+			tomCopy.setEmail("thomas.smith@gmail.com");
+
+			repository.save(tomCopy);
+		}
+
+		System.out.println("------------------------------Custom Queries-------------------------");
+
+		repository.findStudentByEmail("thomas.smith@gmail.com")
+				.ifPresentOrElse(
+						System.out::println,
+						()-> System.out.println("Not Found")
+				);
+
+		System.out.println("-----------------------------------------");
+
+		List<Student> list=repository.
+				findStudentsByFirstNameAndAgeGreaterThanEqual("Alex",21);
+
+		list.forEach(System.out::println);
+
+		System.out.println("-----------------------------------------");
+
+		List<Student> list1=repository.
+				findStudentsByFirstNameOrAgeGreaterThanEqual("Alex",21);
+
+		list1.forEach(System.out::println);
+
+		System.out.println("----------- QUERIES----------------------------");
+		repository.selectStudentByEmail("alex.holmes@gmail.com")
+				.ifPresentOrElse(
+						System.out::println,
+						()-> System.out.println("Not Found")
+				);
+
 	}
 
 }

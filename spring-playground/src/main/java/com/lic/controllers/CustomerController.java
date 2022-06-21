4f76a13ecf9773f23d5f4ed4c84e877d9185a33c
codewithz <zartab@codewithz.com>
2022-06-21 11:27:02 +0530

    Successful Response with Payload

--- spring-playground/src/main/java/com/lic/controllers/CustomerController.java
+++ spring-playground/src/main/java/com/lic/controllers/CustomerController.java
@@ -2,7 +2,10 @@ package com.lic.controllers;
 
 import java.util.List;
 
+import com.lic.config.ApiSuccessPayload;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.lic.model.Customer;
@@ -24,21 +27,32 @@ public class CustomerController {
 	}
 
 	@GetMapping("/{customerId}")
-	public Customer getCustomer(@PathVariable Long customerId) {
+	public ResponseEntity<ApiSuccessPayload> getCustomer(@PathVariable Long customerId) {
+
 		Customer c=customerService.getCustomerById(customerId);
-		return c;
+		HttpStatus status=HttpStatus.OK;
+		ApiSuccessPayload payload=ApiSuccessPayload.build(c,"Customer Found",status);
+		ResponseEntity<ApiSuccessPayload> response=new ResponseEntity<>(payload,status);
+		return response;
 	}
 	
 	@GetMapping
-	public List<Customer> getCustomers(){
+	public ResponseEntity<ApiSuccessPayload> getCustomers(){
 		List<Customer> list=customerService.getCustomers();
-		return list;
+		HttpStatus status=HttpStatus.OK;
+		ApiSuccessPayload payload=ApiSuccessPayload.build(list,"Customers List Found",status);
+		ResponseEntity<ApiSuccessPayload> response=new ResponseEntity<>(payload,status);
+		return response;
+
 	}
 
 	@PostMapping
-	public String addCustomer(@Valid  @RequestBody Customer customer){
-		System.out.println(customer);
-		return "OK";
+	public ResponseEntity<ApiSuccessPayload> addCustomer(@Valid  @RequestBody Customer customer){
+		HttpStatus status=HttpStatus.CREATED;
+		ApiSuccessPayload payload=ApiSuccessPayload.build("OK","OK",status);
+		ResponseEntity<ApiSuccessPayload> response=new ResponseEntity<>(payload,status);
+		return response;
+
 	}
 	
 	

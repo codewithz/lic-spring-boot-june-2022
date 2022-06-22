ef7cd6c7a8cc3cab4181010ba5c1332034218cf5
codewithz <zartab@codewithz.com>
2022-06-22 16:00:02 +0530

    ManyToMany: Creating Composite Key using @Embedable

---
+++ spring-playground/src/main/java/com/lic/model/Enrollment.java
@@ -0,0 +1,11 @@
+//package com.lic.model;
+//
+//import javax.persistence.Entity;
+//import javax.persistence.Table;
+//
+//@Entity(name="Enrollment")
+//@Table(name="enrollment")
+//public class Enrollment {
+//
+//
+//}

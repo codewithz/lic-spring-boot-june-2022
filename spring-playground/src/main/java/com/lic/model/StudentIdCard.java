96b21aca85daaf3ab7a9f4a59fd46689d018293f
codewithz <zartab@codewithz.com>
2022-06-22 10:16:31 +0530

    OneToOne: Created StudentIdCard with @OneToOne and @JoinColumn

---
+++ spring-playground/src/main/java/com/lic/model/StudentIdCard.java
@@ -0,0 +1,80 @@
+package com.lic.model;
+
+import javax.persistence.*;
+
+@Entity(name="StudentIdCard")
+@Table(name = "student_id_card")
+public class StudentIdCard {
+
+    @Id
+    @SequenceGenerator(
+            name = "student_id_card_sequence",
+            sequenceName = "student_id_card_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "student_id_card_sequence"
+    )
+    @Column(
+            name = "id",
+            updatable = false
+    )
+    private Long id;
+
+    @Column(
+            name="card_number",
+            nullable = false,
+            length = 15
+    )
+    private String cardNumber;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(
+            name="student_id",
+            referencedColumnName ="id"
+    )
+    private Student student;
+
+    public StudentIdCard() {
+    }
+
+    public StudentIdCard( String cardNumber, Student student) {
+
+        this.cardNumber = cardNumber;
+        this.student = student;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    @Override
+    public String toString() {
+        return "StudentIdCard{" +
+                "id=" + id +
+                ", cardNumber='" + cardNumber + '\'' +
+                ", student=" + student +
+                '}';
+    }
+}
